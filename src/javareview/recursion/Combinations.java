f46758c3ed031d863c780413a262e7ec8abf7d9d
Guodong Huang <ghuang.dev@gmail.com>
2019-03-14 15:50:33 -0400

    refactor Combinations

--- src/javareview/recursion/Combinations.java
+++ src/javareview/recursion/Combinations.java
@@ -1,5 +1,6 @@
 package javareview.recursion;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,36 +12,48 @@ public class Combinations {
    * @param data
    * @param n
    */
-  public void combinations(List<Integer> data, int n) {
+  public void combinations(List<Integer> selected, List<Integer> data, int n) {
     // initial value for recursion
     // how to select element
     // how to output
 
-    if (data.isEmpty()) {
-      if (n == 0) {
-        // output empty list
-      }
-      return;
-    }
-
-    if (n < 0) {
-      return;
-    }
-
     if (n == 0) {
-      // output empty list
+      // output all selected elements + empty list
+      for (Integer i : selected) {
+        System.out.print(i);
+        System.out.print(" ");
+      }
+      System.out.println();
+      return;
+    }
+
+    if (data.isEmpty()) {
       return;
     }
 
     // select element 0
-    combinations(data.subList(1, data.size()), n - 1);
+    selected.add(data.get(0));
+    combinations(selected, data.subList(1, data.size()), n - 1);
 
     // un-select element 0
-    combinations(data.subList(1, data.size()), n);
+    //    selected.remove(data.get(0));
+    selected.remove(selected.size() - 1);
+    combinations(selected, data.subList(1, data.size()), n);
   }
 
   public static void main(String[] args) {
     Combinations comb = new Combinations();
-    comb.combinations(Arrays.asList(1, 2, 3, 4), 2);
+    comb.combinations(new ArrayList<>(), Arrays.asList(1, 2, 3, 4), 2);
+    System.out.println("====== list select 2 =========");
+    comb.combinations(new ArrayList<>(), Arrays.asList(), 2);
+    System.out.println("====== empty list select 2 =========");
+    comb.combinations(new ArrayList<>(), Arrays.asList(), 1);
+    System.out.println("======= empty list select 1 ========");
+    comb.combinations(new ArrayList<>(), Arrays.asList(1, 2, 3, 4), 1);
+    System.out.println("====== list select 1 =========");
+    comb.combinations(new ArrayList<>(), Arrays.asList(1, 2, 3, 4), 0);
+    System.out.println("====== list select o =========");
+    comb.combinations(new ArrayList<>(), Arrays.asList(1, 2, 3, 4, 5, 6, 7), 5);
+    System.out.println("====== list select 5 =========");
   }
 }
